d346e472482cc76a275b90dba20170cd1dc6f15b
pankowskitomasz <pankowski.email@gmail.com>
2022-03-01 22:03:10 +0200

    routing

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,22 @@
 angular.module("app").config(function($routeProvider){
     $routeProvider.when("/",{
-        templateUrl: "app/templates/comingsoon.html"
+        templateUrl: "app/templates/home.html"
+    })   
+    .when("/about",{
+        templateUrl: "app/templates/about.html"
     })
+    .when("/comingsoon",{
+        templateUrl: "app/templates/comingsoon.html"
+    })  
+    .when("/contact",{
+        templateUrl: "app/templates/contact.html"
+    })   
+    .when("/courses",{
+        templateUrl: "app/templates/courses.html"
+    })
+    .when("/prices",{
+        templateUrl: "app/templates/prices.html"
+    })  
     .when("/privacy",{
         templateUrl: "app/templates/privacy.html"
     })
